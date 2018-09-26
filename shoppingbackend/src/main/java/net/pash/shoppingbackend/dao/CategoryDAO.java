3081e2a5319b4daa40b36ccb2b71aba8b74c02d1
Pash <prashant@velankanigroup.com>
2018-09-26 19:54:14 +0530

    Linked with Backend Project..!

---
+++ shoppingbackend/src/main/java/net/pash/shoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,13 @@
+package net.pash.shoppingbackend.dao;
+
+import java.util.List;
+
+import net.pash.shoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+
+	
+	List<Category> list();
+	
+	
+}
---
+++ shoppingbackend/src/main/java/net/pash/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -0,0 +1,48 @@
+package net.pash.shoppingbackend.daoimpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import net.pash.shoppingbackend.dao.CategoryDAO;
+import net.pash.shoppingbackend.dto.Category;
+@Repository("categoryDAO")
+public class CategoryDAOImpl implements CategoryDAO {
+
+	
+	private static List<Category> categories = new ArrayList<>();
+	
+	static {
+		
+		Category category = new Category();
+		category.setId(1);
+		category.setName("Laptop");
+		category.setDescription("This is description of Laptop");
+		category.setImageURL("CAT_01.jpg");
+		categories.add(category);
+		
+		category = new Category();
+		category.setId(2);
+		category.setName("Mobile");
+		category.setDescription("This is description of Mobile");
+		category.setImageURL("CAT_02.jpg");
+		categories.add(category);
+		
+		category = new Category();
+		category.setId(3);
+		category.setName("Television");
+		category.setDescription("This is description of Television");
+		category.setImageURL("CAT_03.jpg");
+		categories.add(category);
+	}
+	
+	
+	
+	@Override
+	public List<Category> list() {
+		
+		return categories;
+	}
+
+}
